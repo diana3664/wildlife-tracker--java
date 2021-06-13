5161773c0fc87ec579dc26109341c056b458fa5b
diana3664 <dianamus2017@gmail.com>
2021-06-13 17:51:50 +0300

    create a first instantof Animal test and constractor

--- src/main/java/Animal.java
+++ src/main/java/Animal.java
@@ -1,2 +1,13 @@
+
+
+
+
 public class Animal {
+    private String name;
+    private int id;
+
+    public Animal (String name){
+        this.name = name;
+        this.id = id;
+    }
 }
--- src/test/java/AnimalTest.java
+++ src/test/java/AnimalTest.java
@@ -1,2 +1,14 @@
+import org.junit.*;
+import static org.junit.Assert.*;
+
+
+
 public class AnimalTest {
+
+    @Test
+            public void Animal_instantiateCorrectly_true(){
+        Animal testAnimal = new Animal ("Zebra");
+        assertEquals(true, testAnimal instanceof Animal);
+    }
+
 }

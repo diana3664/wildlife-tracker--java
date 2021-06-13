0d2ec2e43d72e606be69c2ecd063e0f743374417
diana3664 <dianamus2017@gmail.com>
2021-06-13 18:15:28 +0300

    add an overide to our constructor to make them equal

--- src/main/java/Animal.java
+++ src/main/java/Animal.java
@@ -10,4 +10,29 @@ public class Animal {
         this.name = name;
         this.id = id;
     }
+
+    public String getName() {
+        return name;
+    }
+
+    //are the same
+
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Animal)) return false;
+
+        Animal animal = (Animal) o;
+
+        if (id != animal.id) return false;
+        return getName().equals(animal.getName());
+    }
+
+    @Override
+    public int hashCode() {
+        int result = getName().hashCode();
+        result = 31 * result + id;
+        return result;
+    }
 }
--- src/test/java/AnimalTest.java
+++ src/test/java/AnimalTest.java
@@ -5,10 +5,28 @@ import static org.junit.Assert.*;
 
 public class AnimalTest {
 
+
+    //instance of class Animal test
     @Test
             public void Animal_instantiateCorrectly_true(){
         Animal testAnimal = new Animal ("Zebra");
         assertEquals(true, testAnimal instanceof Animal);
+    }
+
+    //returns name
+    @Test
+    public void Animal_returnsNameCorrectly_true(){
+        Animal testAnimal = new Animal ("Zebra");
+        assertEquals("Zebra", testAnimal.getName());
+    }
+
+    //if they are the same
+    @Test
+    public void Animal_areEqual_true(){
+        Animal testAnimal = new Animal ("Zebra");
+        Animal secondAnimal = new Animal ("Zebra");
+
+        assertTrue(testAnimal.equals(secondAnimal));
     }
 
 }

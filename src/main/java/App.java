827f819230293f8fe9069fc9e2cd3143448eb3d8
diana3664 <dianamus2017@gmail.com>
2021-06-14 23:41:01 +0300

    add a record.hbs

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -27,8 +27,31 @@ public class App {
             model.put("animals", Animal.all());
             model.put("endangeredAnimals", Endengered.all());
 
-            return new ModelAndView(model, "index.hbs");
+            return new ModelAndView(model, "animalForm.hbs");
         }, new HandlebarsTemplateEngine());
 
+
+        post("/animal/new", (request, response) -> {
+            Map<String, Object> model = new HashMap<String, Object>();
+            boolean endangered = request.queryParamsValues("endangered") != null;
+            if (endangered) {
+                String name = request.queryParams("name");
+                String health = request.queryParams("health");
+                String age = request.queryParams("age");
+                Endengered endangeredAnimal = new Endengered(name, health, age);
+                endangeredAnimal.save();
+                model.put("animals", Animal.all());
+                model.put("endangeredAnimals", Endengered.all());
+            } else {
+                String name = request.queryParams("name");
+                Animal animal = new Animal(name);
+                animal.save();
+                model.put("animals", Animal.all());
+                model.put("endangeredAnimals", Endengered.all());
+
+            }
+            response.redirect("/records.hbs");
+            return null;
+        });
     }
-}
+    }

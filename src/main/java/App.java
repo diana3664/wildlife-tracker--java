939824a459f27b1034121d36372183530579fa8a
diana3664 <dianamus2017@gmail.com>
2021-06-15 10:32:58 +0300

    change our tamplates from handle bars to vtl

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -18,7 +18,36 @@ public class App {
             model.put("animals", Animal.all());
             model.put("endangeredAnimals", Endengered.all());
             model.put("sightings", Sighting.all());
-            return new ModelAndView(model, "index.hbs");
+            return new ModelAndView(model, "index.vtl");
+        }, new HandlebarsTemplateEngine());
+
+        post("/endangered_sighting", (request, response) -> {
+            Map<String, Object> model = new HashMap<String, Object>();
+            String rangerName = request.queryParams("rangerName");
+            int animalIdSelected = Integer.parseInt(request.queryParams("endangeredAnimalSelected"));
+            String latLong = request.queryParams("latLong");
+            Sighting sighting = new Sighting(animalIdSelected, latLong, rangerName);
+            sighting.save();
+            model.put("sighting", sighting);
+            model.put("animals", Endengered.all());
+            String animal = Endengered.find(animalIdSelected).getName();
+            model.put("animal", animal);
+            return new ModelAndView(model, "success.vtl");
+        }, new HandlebarsTemplateEngine());
+
+        post("/sighting", (request, response) -> {
+            Map<String, Object> model = new HashMap<String, Object>();
+            String rangerName = request.queryParams("rangerName");
+            int animalIdSelected = Integer.parseInt(request.queryParams("animalSelected"));
+            String latLong = request.queryParams("latLong");
+            Sighting sighting = new Sighting(animalIdSelected, latLong, rangerName);
+            sighting.save();
+            model.put("sighting", sighting);
+            model.put("animals", Animal.all());
+            String animal = Animal.find(animalIdSelected).getName();
+            model.put("animal", animal);
+            model.put("template", "templates/success.vtl");
+            return new ModelAndView(model, "success.vtl");
         }, new HandlebarsTemplateEngine());
 
 
@@ -27,7 +56,7 @@ public class App {
             model.put("animals", Animal.all());
             model.put("endangeredAnimals", Endengered.all());
 
-            return new ModelAndView(model, "animalForm.hbs");
+            return new ModelAndView(model, "animalForm.vtl");
         }, new HandlebarsTemplateEngine());
 
 
@@ -50,8 +79,28 @@ public class App {
                 model.put("endangeredAnimals", Endengered.all());
 
             }
-            response.redirect("/records.hbs");
+            response.redirect("/");
             return null;
         });
+
+
+        get("/animal/:id", (request, response) -> {
+            Map<String, Object> model = new HashMap<String, Object>();
+            Animal animal = Animal.find(Integer.parseInt(request.params("id")));
+            model.put("animal", animal);
+            return new ModelAndView(model, "animal.vtl");
+        }, new HandlebarsTemplateEngine());
+
+        get("/endangered_animal/:id", (request, response) -> {
+            Map<String, Object> model = new HashMap<String, Object>();
+            Endengered endangeredAnimal = Endengered.find(Integer.parseInt(request.params("id")));
+            model.put("endangeredAnimal", endangeredAnimal);
+            return new ModelAndView(model, "endangered_animal.vtl");
+        }, new HandlebarsTemplateEngine());
+
+        get("/error", (request, response) -> {
+            Map<String, Object> model = new HashMap<String, Object>();
+            return new ModelAndView(model, "error.hbs");
+        }, new HandlebarsTemplateEngine());
     }
-    }
+}
